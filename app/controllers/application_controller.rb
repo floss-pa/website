9742784597c46ef363bdb48ef46b5c9b14c21ccb
Alejandro <aeperezt@fedoraproject.org>
2018-02-25 20:07:16 -0500

    added upload pic on user edit and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,5 +30,6 @@ class ApplicationController < ActionController::Base
   end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:avatar])
   end
 end
